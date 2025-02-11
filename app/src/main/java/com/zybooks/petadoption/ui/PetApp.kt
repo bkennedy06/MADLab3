2f11b2754c0321dbb600c90dcbe6399be56e133d
Bryce Kennedy <bkenne06@calpoly.edu>
2025-02-10 18:19:17 -0800

    PetApp function added

--- app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -46,12 +46,49 @@ import com.zybooks.petadoption.data.PetGender
 import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 import kotlinx.serialization.Serializable
 
+sealed class Routes {
+   @Serializable
+   data object List
 
+   @Serializable
+   data object Detail
+
+   @Serializable
+   data object Adopt
+}
 @Composable
 fun PetApp(
    petViewModel: PetViewModel = viewModel()
 ) {
-   Text("To be implemented...")
+   val navController = rememberNavController()
+
+   NavHost(
+      navController = navController,
+      startDestination = Routes.List
+   ) {
+      composable<Routes.List> {
+         ListScreen(
+            petList = petViewModel.petList,
+            onImageClick = { pet ->
+               petViewModel.selectedPet = pet
+               navController.navigate(Routes.Detail)
+            }
+         )
+      }
+      composable<Routes.Detail> {
+         DetailScreen(
+            pet = petViewModel.selectedPet,
+            onAdoptClick = {
+               navController.navigate(Routes.Adopt)
+            },
+         )
+      }
+      composable<Routes.Adopt> {
+         AdoptScreen(
+            pet = petViewModel.selectedPet,
+         )
+      }
+   }
 }
 
 @Composable
