378130a92d3b2585bcb98e438125e04fef98e401
Bryce Kennedy <bkenne06@calpoly.edu>
2025-02-10 17:43:28 -0800

    ListScreen and Imports added to PetApp.kt

--- app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -1,20 +1,89 @@
 package com.zybooks.petadoption.ui
 
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.zybooks.petadoption.data.Pet
+import com.zybooks.petadoption.data.PetDataSource
+import com.zybooks.petadoption.data.PetGender
+import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
+import kotlinx.serialization.Serializable
+
 
 @Composable
 fun PetApp(
-   modifier: Modifier = Modifier,
    petViewModel: PetViewModel = viewModel()
 ) {
    Text("To be implemented...")
+}
+
+@Composable
+fun ListScreen(
+   petList: List<Pet>,
+   onImageClick: (Pet) -> Unit,
+   modifier: Modifier = Modifier
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Find a Friend"
+         )
+      }
+   ) { innerPadding ->
+      LazyVerticalGrid(
+         columns = GridCells.Adaptive(minSize = 128.dp),
+         contentPadding = PaddingValues(0.dp),
+         modifier = modifier.padding(innerPadding)
+      ) {
+         items(petList) { pet ->
+            Image(
+               painter = painterResource(id = pet.imageId),
+               contentDescription = "${pet.type} ${pet.gender}",
+               modifier = Modifier.clickable(
+                  onClick = { onImageClick(pet) },
+                  onClickLabel = "Select the pet"
+               )
+            )
+         }
+      }
+   }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
